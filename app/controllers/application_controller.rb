748c7720b3a7636ecdb5328bd1a5d2a992f96630
Javier Jimenez and Mark Helt <javier+mark@>
2017-11-29 11:05:49 -0800

    avatar added to profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
     protected
 
       def configure_permitted_parameters
-          devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password])
-          devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :current_password, :is_female, :date_of_birth])
+          devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :is_female, :date_of_birth, :avatar])
+          devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :current_password, :is_female, :date_of_birth, :avatar])
       end
 end
